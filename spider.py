56936502615bf4bcc35964bdeebb0c08f77274f6
zatricion <zatricion@gmail.com>
2013-08-17 02:58:56 -0400

    create table if need be

--- spider.py
+++ spider.py
@@ -13,5 +13,6 @@ from models import *
 from views import *
 
 if __name__ == "__main__":
+    Connection.create_table(fail_silently=True)
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
