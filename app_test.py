75b7f7dfae8118d139179352e4bd55353a5b2374
zatricion <zatricion@gmail.com>
2013-08-19 03:29:44 -0400

    remove typo

--- app_test.py
+++ app_test.py
@@ -20,7 +20,7 @@ class CloudTestCase(unittest.TestCase):
         req = demjson.encode({"//en.wikipedia.org/wiki/Thuringia":[{"in_node":"//en.wikipedia.org/wiki/Wartburg","timestamp":1376896096967}],"//en.wikipedia.org/wiki/Wartburg":[{"in_node":"//en.wikipedia.org/wiki/Frederick_II,_Margrave_of_Meissen","timestamp":1376895634637}],"//en.wikipedia.org/wiki/Frederick_II,_Margrave_of_Meissen":[{"in_node":"//en.wikipedia.org/wiki/Elisabeth_of_Meissen","timestamp":1376895632684}],"//en.wikipedia.org/wiki/Elisabeth_of_Meissen":[{"in_node":"","timestamp":1376895596200}]})
 
         response = tester.post('/add_mark', req)          
-"
+
         self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
