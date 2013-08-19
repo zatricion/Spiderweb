b6ae706941cdc9155350f7421d5c3522a66356d1
zatricion <zatricion@gmail.com>
2013-08-19 03:14:17 -0400

    okay, now debug statements are gone

--- views.py
+++ views.py
@@ -17,21 +17,18 @@ def home():
 
 @app.route("/add_mark", methods=['POST'])
 def serve():
-    say = "old"
     Connection.create_table(fail_silently=True)
 
     link_dict = demjson.decode(request.stream.read())
     for to_url in link_dict:
-        say = "older"
         from_url = link_dict[to_url][0]['in_node']
         try:
             r = Connection.get(Connection.from_url == from_url, Connection.to_url == to_url)
             r.count += 1
         except Connection.DoesNotExist:
-            say = "updating"
             r = Connection(from_url = from_url, to_url = to_url, count = 1)
         r.save()
-    resp = Response(say, status=200, mimetype='application/json')
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 @app.route("/clouds.json")
