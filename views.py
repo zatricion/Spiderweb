6c3fa2b3a06d24539d195947dd59e528daae71d9
zatricion <zatricion@gmail.com>
2013-08-20 20:29:32 -0400

    new tree recursion attempt

--- views.py
+++ views.py
@@ -24,10 +24,10 @@ def home():
 
 @app.route("/", methods=['POST'])
 def bubbles():
-  req = request.form
-  word = req.get('word', None)
-  node_list = get_bubbles(word)
-  return node_list
+    req = request.form
+    word = req.get('word', None)
+    node_list = get_bubbles(word)
+    return node_list
   #return render_template('bubbles.html', nodes=node_list)
 
 @app.route("/add_mark", methods=['POST'])
@@ -40,8 +40,8 @@ def serve():
             r.count += 1
         except Connection.DoesNotExist:
             r = Connection(from_url = from_url, to_url = to_url, count = 1)
-        r.save()
-    resp = Response(status=200, mimetype='application/json')
+            r.save()
+            resp = Response(status=200, mimetype='application/json')
     return resp
 
 # Helper Functions
@@ -49,30 +49,57 @@ def serve():
 # Go through the database and assemble a list of urls
 # rank them by weighted distance from search url
 def get_bubbles(url):
-  node_dict = {}
+    node_dict = {}
+    
+    # Put entries in the table from "right-hand" path
+    right_dist = 0
+    right_url = url
+    
+    # wDist is the weighted distance (accumulates edge weights)
+    def distance_dict(node, wDist=None):
+        wDist = 0 if wDist == None else wDist
+        for child in get_children(node):
+            wDist += (1.0 / child.count)
+            if !child.is_leaf:
+                for each in distance_dict(child, wDist):
+                    yield each
+        yield child.url, wDist
+  
+def weighted_node_distances(rootNode):
+    distances = dict(distance_dict(rootNode))
+    return demjson.encode(distances)
 
-  # Put entries in the table from "right-hand" path
-  right_dist = 0
-  right_url = url
-  while True:
-    try:
-      right = Connection.get(Connection.from_url == right_url)
-      right_dist += (1.0 / right.count)
-      right_url = right.to_url
-      node_dict[right_url] = right_dist
-    except Connection.DoesNotExist:
-      break
+# node Class
+class Node:
+    def __init__(self, url, count):
+        self.url = url
+        self.count = count
+            
+        def is_leaf(self):
+            return bool(get_children(self))
 
-  # Put entries in the table from "left-hand" path
-  left_dist = 0
-  left_url = url
-  while True:
-    try:
-      left = Connection.get(Connection.to_url == left_url)
-      left_dist += (1.0 / left.count)
-      left_url = left.from_url
-      node_dict[left_url] = left_dist
-    except Connection.DoesNotExist:
-      break
-
-  return demjson.encode(node_dict)
+# get the children of a node from its url
+def get_children(node):
+      url = node.url
+                  
+      # Populate children list
+      children = from_db('left', url).extend(from_db('right', url))
+      return children
+      
+      
+def from_db(side, url)):
+      children = []
+      right = True if side == 'right' else False
+      thisUrl = Connection.from_url if right else Connection.to_url
+      while True:
+            try:
+                  nextCon = Connection.get(thisUrl == url)
+                  nextUrl = nextCon.to_url if right else nextCon.from_url
+                  if nextUrl in nodes:
+                        break
+                  url = nextUrl
+                  children.append(Node(url, nextCon.count))
+                  
+            except Connection.DoesNotExist:
+                  break
+      return children
