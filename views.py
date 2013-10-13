2e0a90ae99a6e5a8716ed0737980d81222f07f38
zatricion <zatricion@gmail.com>
2013-10-12 18:22:34 -0700

    attempt to solve no project issue with default var

--- views.py
+++ views.py
@@ -45,7 +45,7 @@ def test():
     return demjson.encode(weighted_node_distances(word))
 
 @app.route("/add_mark/<project>", methods=['POST'])
-def serve(project):
+def serve(project=''):
     link_dict = demjson.decode(request.stream.read())
     for to_url in link_dict:
         from_url = link_dict[to_url][0]['in_node']
